40f56dab394f176bad0c3b0ba1acf222272c9d18
usuario <usuario@dominio.com>
2026-04-30 20:24:50 +0200

    Corregir índice de password a 1 (SELECT id, password)

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def login():
         print(f"LOGIN DEBUG: usuario={usuario}, password={password}, user={user}")
         conn.close()
 
-        if user and user[2] == password:
+        if user and user[1] == password:
             session["usuario"] = usuario
             flash("Bienvenido " + usuario, "success")
             return redirect(url_for("admin"))
